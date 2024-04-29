2e73e7189a693ca304a85690d22cac5086ae00ac
KommineniCharan <charank347@gmail.com>
2024-04-29 08:08:24 +0530

    Javascript Introduction

---
+++ Javascript/indentifier.js
@@ -0,0 +1,8 @@
+var id = 101;
+console.log(id);
+
+var name = "Good Morning";
+console.log(name);
+
+var iff = "How are You";
+console.log(iff);
---
+++ Javascript/introduction.js
@@ -0,0 +1,23 @@
+console.log("Hello World, Good Morning");
+
+var id = 101;
+console.log("My Id is: " + id);
+
+var a = 30;
+var b = 50;
+
+console.log(a + b);
+
+var c = "Mahesh";
+var d = "Saurabh";
+
+console.log(c + d);
+
+var e = "Biswal";
+var f = 9;
+
+console.log(e + f);
+
+var g = "2";
+var h = 2;
+console.log(g + h);
