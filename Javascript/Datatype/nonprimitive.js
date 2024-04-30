80f0693c515a598248ba67cd1d8e56c6f9e2c5ab
KommineniCharan <charank347@gmail.com>
2024-04-30 08:07:11 +0530

    datatypes

---
+++ Javascript/Datatype/nonprimitive.js
@@ -0,0 +1,22 @@
+//array
+var size = [30, 32, 34, 36, 38];
+console.log(size);
+console.log(typeof size);
+console.log(size[4]);
+console.log(size[8]);
+console.log(first);
+
+//object
+var emp = {
+  id: 101,
+  name: "Sourabh",
+  address: "Bangalore",
+  salary: 45000,
+};
+
+console.log(emp);
+console.log(typeof emp);
+console.log(emp.id);
+console.log(emp.name);
+console.log(emp.address);
+console.log(emp.salary);
---
+++ Javascript/Datatype/primitive.js
@@ -0,0 +1,32 @@
+//Number
+var id = 101;
+console.log("Id is : " + id);
+console.log(typeof id);
+
+//String
+var name = "Mahesh";
+console.log("Name is " + name);
+console.log(typeof name);
+
+//Boolean
+var discount = true;
+console.log("Discount is : " + discount);
+console.log(typeof discount);
+
+//undefined
+var gst;
+console.log(typeof gst);
+
+//Null
+var pan = null;
+console.log(typeof pan);
+
+//Symbol
+var sym = Symbol("description");
+console.log(sym);
+console.log(typeof sym);
+
+//BigInt
+var total = 999999988888888555n;
+console.log(total);
+console.log(typeof total);
